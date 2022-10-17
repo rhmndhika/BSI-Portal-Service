e15efa912c6282702f23bb716578971bed80acf9
rhmndhika <rahmandhika@student.umn.ac.id>
2022-10-17 17:02:31 +0700

    Add page for admin

---
+++ client/src/Component/Admin/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Appbar from '../Appbar/Appbar.tsx';
+const Admin = () => {
+  return (
+    <div>
+        <Appbar />
+        <h1>ADMIN</h1>
+    </div>
+  )
+}
+
+export default Admin
