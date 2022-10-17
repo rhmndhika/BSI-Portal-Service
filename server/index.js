13cd7d78388afc88238fbfa0a16a7097d77380e6
rhmndhika <rahmandhika@student.umn.ac.id>
2022-10-17 15:47:29 +0700

    fixing cookie-session

--- server/index.js
+++ server/index.js
@@ -1,34 +1,41 @@
-const express = require('express'); 
-const app = express();              
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
-const multer = require('multer');
+const app = express();
 const path = require('path');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const helmet = require('helmet');
+const hpp = require('hpp');
 const mongoose = require('mongoose');
 const jwt = require("jsonwebtoken");
 const MemoryStore = require('memorystore')(session)
-const UserModel = require("./models/Users.js");
+
 
 require('dotenv').config();
+
+
+const UserModel = require("./models/Users.js");
+
 
 const CONNECTION_URL =  process.env.MONGODB_HOST
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser : true
-});
-
+}
+);
 
 app.use(
     cors({
-    origin: ["http://localhost:3000", "https://empty-test-project.herokuapp.com"],
+    origin: ["https://bsivendorregistration.netlify.app", ,"https://bsivendor-registration.herokuapp.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
 
 app.set("trust proxy", 1);
+
 app.use(helmet());
 app.use(hpp());
 app.use(express.json());
@@ -39,24 +46,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use(session({
-  cookie:{
-      secure: true,
-      sameSite: "none",
-      httpOnly: true,
-      maxAge:10 * 10 * 24 * 60
-         },
-  key: process.env.COOKIE_KEY,
-  store: new MemoryStore({
-      checkPeriod: 86400000 // prune expired entries every 24h
-  }),
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: false,
-  httpOnly : true
-  }));
+    cookie:{
+        secure: true,
+        sameSite: "none",
+        maxAge:10 * 10 * 24 * 60
+           },
+    key: process.env.COOKIE_KEY,
+    store: new MemoryStore({
+        checkPeriod: 86400000 // prune expired entries every 24h
+      }),
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: false,
+    }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000", "https://empty-test-project.herokuapp.com");
+    res.setHeader("Access-Control-Allow-Origin", "https://bsivendorregistration.netlify.app", "https://bsivendor-registration.herokuapp.com");
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
@@ -106,6 +111,7 @@ app.post("/register", async (req, res) => {
     });
 });
 
+
 app.post("/login", (req, res) => {
     UserModel.findOne({ email: req.body.email }).then((user) => {
         bcrypt.compare(req.body.password, user.password).then((passwordCheck) => {
@@ -154,7 +160,8 @@ app.post("/login", (req, res) => {
 app.get("/login", (req, res) => {
 
     if(req.session.email) {
-        res.send({loggedIn: true, email: req.session.email, role : req.session.role })
+            res.send({loggedIn: true, email: req.session.email, role : req.session.role })
+       
     } else {
         res.send({loggedIn: false})
     }
@@ -163,10 +170,12 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-  res.clearCookie(process.env.COOKIE_KEY)
-  return res.redirect("/");
+   res.clearCookie(process.env.COOKIE_KEY)
+   return res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3001, () => {           
-    console.log(`On Air`); 
+
+
+app.listen(process.env.PORT || 3001 , ()=> {
+    console.log(`running on port`)
 });
