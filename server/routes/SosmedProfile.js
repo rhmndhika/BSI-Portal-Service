903fa0376f11140ee5e3a41a96a79815bbfe16aa
rhmndhika <rahmandhika@student.umn.ac.id>
2022-11-28 15:30:41 +0700

    add mutler in sosmed profile routes

--- server/routes/SosmedProfile.js
+++ server/routes/SosmedProfile.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
 const SosmedProfileModel = require("../models/SosmedProfile.js");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() +  path.extname(file.originalname))
+  }
+});
+
+const upload = multer({storage: storage});
 
 const createProfile = async (req, res) => {
   
