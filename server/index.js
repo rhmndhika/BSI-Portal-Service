727bc1833a080670d4e760acb2b7f88466e9784f
rhmndhika <rahmandhika@student.umn.ac.id>
2022-12-05 11:59:53 +0700

    fix some bugs

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://bsi-portal-supplier.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   },
 });
