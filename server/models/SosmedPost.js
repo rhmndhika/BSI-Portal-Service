cc3a3f5bdc324daec58322cd81fb67551c661bcd
rhmndhika <rahmandhika@student.umn.ac.id>
2022-12-20 09:13:30 +0700

    V3

--- server/models/SosmedPost.js
+++ server/models/SosmedPost.js
@@ -2,26 +2,35 @@ const mongoose = require("mongoose");
 
 const SosmedPostSchema = new mongoose.Schema({
    Username : {
-          type : String
+       type : String
    }, 
    Content : {
-          type : String
+       type : String
    },     
    Documents : {
-          type : String
+       type : String
    },
    Author : { 
-          type : mongoose.Schema.Types.ObjectId, 
-          ref : 'sosmedprofiles'
+       type : mongoose.Schema.Types.ObjectId, 
+       ref : 'sosmedprofiles'
    },
    Comments : [{
-          Text : String,
-          PostedBy : { type : mongoose.Schema.Types.ObjectId, ref : "sosmedprofiles"}
+       Text : {
+              type : String
+       },
+       PostedBy : { 
+              type : mongoose.Schema.Types.ObjectId, 
+              ref : "sosmedprofiles"
+       }
    }],
    Likes : [{
           type : mongoose.Schema.Types.ObjectId,
           ref : "sosmedprofiles"
-   }]
+   }],
+   PostedBy : {
+          type : mongoose.Schema.Types.ObjectId,
+          ref : "sosmedprofiles"
+   }
 }, {timestamps : true});
 
 
